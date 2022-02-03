c835c395a6ea75138469a81b7bfd9ee1a8bfaf60
Kelvin C Okpala <98800201+Kelvin-Okpala@users.noreply.github.com>
2022-02-03 01:15:40 +0100

    Update covid portfolio project with actual script.sql

--- covid portfolio project with actual script.sql
+++ covid portfolio project with actual script.sql
@@ -174,3 +174,57 @@ WHERE dea.continent is not null
 
 SELECT *
 FROM PercentPopulationVaccinated;
+
+
+/*
+
+Queries used for Tableau Project
+
+*/
+
+
+
+-- 1. 
+
+SELECT SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS int)) AS total_deaths, SUM(CAST(new_deaths AS int))/SUM(New_Cases)*100 AS 
+  DeathPercentage
+FROM PortfolioProject..[covid deaths]
+--WHERE location like '%states%'
+WHERE continent is not null 
+--GROUP BY date
+ORDER BY 1,2;
+
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
+
+
+-- 2. 
+
+-- We take these out as they are not included in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+SELECT location, SUM(CAST(new_deaths AS int)) AS TotalDeathCount
+FROM PortfolioProject..[covid deaths]
+--WHERE location like '%states%'
+WHERE continent is null 
+AND location NOT IN ('World', 'European Union', 'International')
+GROUP BY location
+ORDER BY TotalDeathCount DESC;
+
+
+-- 3.
+
+SELECT Location, Population, MAX(total_cases) AS HighestInfectionCount,  MAX((total_cases/population))*100 AS PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--WHERE location like '%states%'
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected DESC;
+
+
+-- 4.
+
+SELECT Location, Population, date, MAX(total_cases) AS HighestInfectionCount,  MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM PortfolioProject..CovidDeaths
+--WHERE location like '%states%'
+GROUP BY Location, Population, date
+ORDER BY PercentPopulationInfected DESC;
